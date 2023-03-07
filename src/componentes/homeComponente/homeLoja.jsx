088d22c9a4f8b2272a89a14afc22efed21966df1
unknown <andrejoas93@gmail.com>
2023-03-07 18:35:38 -0300

    frutos

--- src/componentes/homeComponente/homeLoja.jsx
+++ src/componentes/homeComponente/homeLoja.jsx
@@ -1,14 +1,32 @@
 import HeaderComponente from "../headerLojacomponente"
 import Procurando from "../procurandoComponente"
-import "./styles.css"
+import star from "../homeComponente/S.svg"
+import "./styles2.css"
 
 export default function HomeLoja() {
     return (
-        <body>
-            <HeaderComponente />
-            <section>
+        <body className="corpo">
+            <header>
+                <HeaderComponente />
+            </header>
+            <section className="sessao">
                 <Procurando />
 
+                <div className="nomeloja">
+                    <div className="perfilLoja">
+                        <div className="elipse2"></div>
+                        <h3>Nome Loja</h3>
+                    </div>
+                    <div className="divdados">
+                        <div className="dados">
+                            <img src={star}/>
+                            <h2>4.7</h2>
+                        </div>
+                    </div>
+                    <div className="sobre">
+                            <h2>Sobre a loja</h2>
+                    </div>
+                </div>
             </section>
         </body>
     )
