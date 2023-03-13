138e170c739e3658cf589780ddd73f9ec6fd21ad
unknown <andrejoas93@gmail.com>
2023-03-13 10:45:22 -0300

    frutos

--- src/componentes/listaLojaProdutoComponente/index.jsx
+++ src/componentes/listaLojaProdutoComponente/index.jsx
@@ -5,36 +5,63 @@ import "./styles.css";
 function ListaLojaProdutos() {
     return (
         <div className="lista2">
-                <div className="numero2">
-                    <CardLojaProduto />
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                </div>
-                <div className="numero2">
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                </div>
-                <div className="numero2">
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                </div>
-                <div className="numero2">
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                </div>
-                <div className="numero2">
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                    <CardLojaProduto/>
-                </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <br></br>
+
+            <br></br>
+
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <br></br>
+
+            <br></br>
+
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <br></br>
+
+            <br></br>
+            
+           
+            <div className="numero2">
+                <a className="lincagem" href=""><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+            </div>
+            <div className="numero2">
+                <a className="lincagem" href=""><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+            </div>
+            <div className="numero2">
+                <a className="lincagem" href=""><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+            </div>
+            <div className="numero2">
+                <a className="lincagem" href=""><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+            </div>
+            <div className="numero2">
+                <a className="lincagem" href=""><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+                <a className="lincagem" ><CardLojaProduto /></a>
+            </div>
 
         </div>
     )
