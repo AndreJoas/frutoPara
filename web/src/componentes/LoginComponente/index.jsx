2fd282e5e3ab6f0a4736a60b7c1bba4221d0f2ed
Diogo Alvino <diogo.alvino@planium.io>
2023-03-14 09:47:05 -0300

    update: Ajustando css tela de produtos

--- web/src/componentes/LoginComponente/index.jsx
+++ web/src/componentes/LoginComponente/index.jsx
@@ -4,25 +4,21 @@ import { useState } from "react";
 
 
 function LoginComponente() {
-    const [valueLogin, newValueLogin] = useState("")
+    const [email, newValueEmail] = useState("")
 
     return (
         <body>
             <header className="headerLogin">
-            <div className="logoHeaderLogin">
-                <img src={logo} alt="" />
-            </div>
+                <div className="logoHeaderLogin">
+                    <img src={logo} alt="" />
+                </div>
             </header>
             <section className="login">
                 <div className="forme">
                     <h1 className="bem">Bem vindo ao Login</h1>
-                    <div className="perfil">
-                        <button id="bt"><a href="/loginPage" style={{ textDecoration: "none", color: "#000000" }}>Cliente/PF</a></button>
-                        <button id="bt2" ><a href="/lojaLoginPage" style={{ textDecoration: "none", color: "#000000" }}>Loja/PJ</a></button>
-                    </div>
                     <div className="campos">
-                        <input className="ip" type="name" placeholder="nome" value={valueLogin} onChange={e => newValueLogin(e.target.value)} />
-                        <input className="ip" type="senha" placeholder="senha" />
+                        <input className="ip" type="text" placeholder="Email" value={email} onChange={e => newValueEmail(e.target.value)} />
+                        <input className="ip" type="password" placeholder="senha" />
                     </div>
                     <a href="/HomeCliente"><button className="entrar">Entrar</button></a>
                 </div>
